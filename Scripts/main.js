b2816aef67eb097300630efc6af2126cfb6d72b0
Danick Ditchou <dn303068@dal.ca>
2025-06-01 21:03:20 -0300

    Added audio file

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,11 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById('audio').play();
+
+function addEvent(){
+    document.getElementById("music").addEventListener("click", play);
+}
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+}

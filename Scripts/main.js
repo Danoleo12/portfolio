7603f0171b1544046ab666755beb9455e0f2ca98
Danick Ditchou <dn303068@dal.ca>
2025-06-13 01:25:49 -0300

    Took and displayed user's name

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,10 @@
+var userName = prompt("What do I call you?");
+if(userName == null || userName == ""){
+    alert("No name provided");
+} else {
+    document.querySelector("h1").textContent = "Hello " + userName + " I am Danick";
+}
+
 alert("Double click on triangle button to play music");
 document.getElementById("year").textContent = new Date().getFullYear();
 

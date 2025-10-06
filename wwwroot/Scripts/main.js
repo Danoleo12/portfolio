b4d2828ebed126b6057e2205a8f7751bb05b839e
Danick Ditchou <dn303068@dal.ca>
2025-10-06 01:18:57 -0300

    I have decided to redesign my Portfolio, finish building and publish it

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -1,19 +1,6 @@
 var userName = prompt("What do I call you?");
-if(userName == null || userName == ""){
-    alert("No name provided");
-    alert("Double click on triangle button to play the background music");
-} else {
-    document.querySelector("h1").textContent = "Hello " + userName + " I am Danick";
-    alert("Hi " + userName + "\nDouble click on triangle button to play the background music");
-}
+
+document.querySelector("h1").textContent = "Hello " + userName + " I am Danick";
 
 
 document.getElementById("year").textContent = new Date().getFullYear();
-
-function addEvent(){
-    document.getElementById("music").addEventListener("click", play);
-}
-function play(){
-    var audio = document.getElementById("audio");
-    audio.play();
-}

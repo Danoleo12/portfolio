ab29cd7238f6911ce4d40a0e56890479f58de280
Danick Ditchou <yamdjioleonel@gmail.com>
2026-01-03 14:33:34 -0400

    Added one video, removed the broken link, and updated the layout. Still have to add my certifications.

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -1,6 +1,27 @@
 var userName = prompt("What do I call you?");
 
-document.querySelector("h1").textContent = "Hello " + userName + " I am Danick";
+document.querySelector("h1").textContent = "Hello " + userName + ", I am Danick";
 
 
 document.getElementById("year").textContent = new Date().getFullYear();
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', async () => {
+        card.classList.toggle('flipped');
+
+        const video = card.querySelector('video');
+
+        if (card.classList.contains('flipped') && video) {
+            try {
+                await video.play(); // must be playing before PiP
+                await video.requestPictureInPicture();
+            } catch (err) {
+            console.error("PiP failed:", err);
+            } 
+        }
+
+        if (!card.classList.contains('flipped') && document.pictureInPictureElement === video) {
+            await document.exitPictureInPicture();
+        }
+    });
+});
